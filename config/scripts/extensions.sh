4f330b9d33c34c7293d0db59be4ccf23cfdfaae7
Joe Ipson <joe@ipson.me>
2024-01-22 16:13:33 -0700

    Some other changes

--- config/scripts/extensions.sh
+++ config/scripts/extensions.sh
@@ -10,17 +10,16 @@ echo 'Installing extensions'
 
 cd /tmp
 
-git clone https://gitlab.com/arcmenu/ArcMenu.git
-cd ArcMenu
-make DESTDIR=/ install
-cd ..
-
-git clone https://github.com/theychx/WorkspaceSwitcherWrapAround.git
-mkdir /usr/share/gnome-shell/extensions/WorkspaceSwitcherWrapAround
-cp WorkspaceSwitcherWrapAround/src/* /usr/share/gnome-shell/extensions/WorkspaceSwitcherWrapAround
+git clone https://github.com/theychx/WorkspaceSwitcherWrapAround.git /usr/share/gnome-shell/extensions/workspace-switch-wraparound@theychx.org
 
 git clone https://github.com/ethnarque/gnome-disable-workspace-animation.git
 cp -r gnome-disable-workspace-animation/disable-workspace-animation@ethnarque /usr/share/gnome-shell/extensions
 
 git clone https://github.com/cleardevice/gnome-disable-workspace-switcher.git
 cp -r gnome-disable-workspace-switcher/disable-workspace-switcher-overlay@cleardevice /usr/share/gnome-shell/extensions
+
+git clone https://gitlab.com/arcmenu/ArcMenu.git
+cd ArcMenu
+make DESTDIR=/ install
+
+dconf update
