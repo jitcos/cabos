a2944c5aefdc9c80ac01360f1163fce0d41313e2
Joe Ipson <joe@ipson.me>
2024-01-24 23:38:23 -0700

    Change how prompt is installed

---
+++ config/scripts/prompt.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+wget https://copr.fedorainfracloud.org/coprs/kylegospo/prompt/repo/fedora-$(rpm -E %fedora)/kylegospo-prompt-fedora-$(rpm -E %fedora).repo?arch=x86_64 -O /etc/yum.repos.d/_copr_kylegospo-prompt.repo
+rpm-ostree override replace \
+  --experimental \
+  --from repo=copr:copr.fedorainfracloud.org:kylegospo:prompt \
+      vte291 \
+      vte-profile
+rpm-ostree install prompt
+rm -f /etc/yum.repos.d/_copr_kylegospo-prompt.repo
