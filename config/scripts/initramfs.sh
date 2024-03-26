1274e11e8c2d5bc98f2034e0c9e09fa9fa55f4e4
Joe Ipson <joe@ipson.me>
2024-03-26 13:56:13 -0600

    Fix build?

--- config/scripts/initramfs.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Tell this script to exit if there are any errors.
-# You should have this in every custom script, to ensure that your completed
-# builds actually ran successfully without any errors!
-set -oue pipefail
-
-rpm-ostree initramfs --enable
